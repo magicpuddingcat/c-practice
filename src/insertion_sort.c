7731941e390f9c11e1c551b20fb6c85372d455a4
magicpuddingcat <danielle_rebecca_9@hotmail.com>
2019-11-27 13:06:29 +1100

    Adding first insertion_sort program

---
+++ src/insertion_sort.c
@@ -0,0 +1,40 @@
+/*
+ * insertion_sort.c
+ *
+ *  Created on: 23 Nov 2019
+ *      Author: danielle
+ */
+
+void insertion_sort(int numarray[], int arraylen);
+
+int main( int argc, const char* argv[] )
+{
+	int a[] = {4,3,1,6};
+	int b[] = {9,5,3,10,1,4,2,11,6};
+
+	insertion_sort(a, 4);
+	insertion_sort(b, 9);
+}
+
+void insertion_sort(int numarray[], int arraylen) {
+
+	for (int current = 1; current < arraylen; current++) {
+		//Maintain a record of the current element in the array
+		int this_element = numarray[current];
+
+		int previous = current - 1;
+
+		while (previous >= 0 && numarray[previous] >= numarray[current]) {
+			numarray[current] = numarray[previous];
+			numarray[previous] = this_element;
+			current = previous;
+			previous = previous - 1;
+		}
+		}
+
+	for (int i = 0; i < arraylen; i++) {
+		printf("%d ", numarray[i]);
+	}
+
+	printf("\n");
+}
