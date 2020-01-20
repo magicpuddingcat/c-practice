0cde508526f6b55fa9d7891180355d0b6345c198
magicpuddingcat <danielle_rebecca_9@hotmail.com>
2020-01-20 15:38:07 +1100

    Now starting the whole thing again with some support from an online tutorial.

--- src/hash_table.c
+++ src/hash_table.c
@@ -10,57 +10,38 @@
 #include <string.h>
 #include <stdlib.h>
 
-int hashfunc(char arr[], int arr_len);
-int find_string_len(char char_array[]);
+#define MAX 10
 
-// Initialise struct to handle key-value pairings
-struct Dict {
-   int index;
+typedef struct {
+   int *index;
    char *key;
-};
+} HTEntry;
+
+typedef struct {
+	int size;
+	int num_entries;
+	HTEntry** entries;
+} HTable;
+
+struct TableEntry* entry_arr[MAX];
 
 int main( int argc, const char* argv[] )
 {
-   //Recording example array of keys
-	char keys1[] = {"ffthd", "HGfftyn", "bcdancH"};
-	int keys1_len = 3;
 
-	struct Dict this_dict[keys1_len];
-
-	//Assign key and index to members of the Dict array and then print them
-	for (int i=0; i < keys1_len; i++) {
-		//Assign the string as key and then assign it an index number
-		this_dict[i].key = &keys1[i]; // @suppress("Field cannot be resolved")
-		this_dict[i].index = hashfunc(keys1[i], find_string_len(keys1[i])); // @suppress("Field cannot be resolved")
-
-		//Now print the array of structs
-		printf("%d |", this_dict[i].index);
-		printf("%f", keys1[i]);
-		printf("\n");
-	}
 }
 
-int find_string_len(char char_array[]) { // @suppress("No return")
-	int i = 0;
-	int index;
-
-	while (char_array[i] != '\0') {
-	    i++;
-	}
-
-	return i;
+int hash(int index) {
+	return index % MAX;
 }
 
-int hashfunc(char arr[], int arr_len) {
-	//Implement a simple hashing function
-	int total = 0;
-	int index;
+void add() {
 
-	for (int i = 0; i<arr_len; i++) {
-		total += arr[i];
-	}
+}
 
-	index = total % arr_len;
+void delete() {
 
-	return index;
+}
+
+void search() {
+
 }
